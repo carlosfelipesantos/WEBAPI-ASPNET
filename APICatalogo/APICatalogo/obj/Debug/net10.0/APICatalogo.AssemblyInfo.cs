4dfe6833620155a4577c47f0c337054daba1c3dd
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-02-03 17:01:02 -0300

    Configurando para EF Core

--- APICatalogo/APICatalogo/obj/Debug/net10.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net10.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22749743c8e17ace1b1aae4c00a04874352c477")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e84e29476f9f5ec3cb4b7697132a9d93cbc90e99")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
