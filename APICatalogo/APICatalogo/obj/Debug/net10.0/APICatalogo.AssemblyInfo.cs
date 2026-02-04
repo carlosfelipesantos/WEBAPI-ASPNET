d35dd88de604e0240335ddd08ce4b280a1adbf60
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-02-04 16:17:55 -0300

    Aplicando Migrations

--- APICatalogo/APICatalogo/obj/Debug/net10.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net10.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bbab24b995fec91bea330f6a05df14d589a22a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f81682ab1fa51008b2483851b35b07f5a9e5672")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
